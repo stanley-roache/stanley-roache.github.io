d254d5f0d1fc42c67abc06b43ea4872cf853adf3
Stanley Roache <stan.roache@gmail.com>
2018-03-21 13:22:30 +1300

    added a few lines about creating a player blob

--- game/game.js
+++ game/game.js
@@ -1,3 +1,5 @@
+// This is the class for an individual blob in the game where blobs eat each other
+
 class Blob {
   constructor (radius, position, velocity, colour) {
     this.radius = radius;
@@ -7,34 +9,40 @@ class Blob {
   }
 
   getAbsVel() {
-    return Math.sqrt(Math.pow(this.velocity.x, 2) + Math.pow(this.velocity.y, 2));
+    return Math.sqrt(Math.pow(this.velocity[0], 2) + Math.pow(this.velocity[1], 2));
   }
 
   // This function updates the blobs position from it's current speed and position 
   move() {
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
+    this.position[0] += this.velocity[0];
+    this.position[1] += this.velocity[1];
     // this.viscosity();
   }
 
   // This function decellerates the blob proportional to it's current velocity
   viscosity() {
-    this.velocity.x *= 0.95;
-    this.velocity.y *= 0.95;
+    this.velocity[0] *= 0.95;
+    this.velocity[1] *= 0.95;
   }
 
   // This function applies a force to the blob and accelerates it
   accelerate(force) {
-    this.velocity.x += force[0];
-    this.velocity.y += force[1];
+    this.velocity[0] += force[0];
+    this.velocity[1] += force[1];
   }
 
   // This function checks if two blobs are contacting each other
   static checkContact (a,b) {
-    return a.radius + b.radius <= Math.sqrt(Math.pow(a.position.x - b.position.x, 2) + Math.pow(a.position.y - b.position.y, 2)); 
+    return a.radius + b.radius <= Math.sqrt(Math.pow(a.position[0] - b.position[0], 2) + Math.pow(a.position[1] - b.position[1], 2)); 
   }
 }
 
+var blobs = [];
+
 document.addEventListener("DOMContentLoaded", function() {
-  
+  var playerBlob = new Blob(
+    10,
+    [50, 50],
+    [3,3],
+  );
 });
