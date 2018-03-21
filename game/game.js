cb18b375235a450ac6cb6b56cd285b354b60936c
Stanley Roache <stan.roache@gmail.com>
2018-03-21 15:34:56 +1300

    display loop is working with one div in action

--- game/game.js
+++ game/game.js
@@ -1,15 +1,25 @@
 // This is the class for an individual blob in the game where blobs eat each other
 
 class Blob {
-  constructor (radius, position, velocity, colour) {
+  constructor (radius, position, velocity) {
     this.radius = radius;
     this.position = position;
     this.velocity = velocity;
-    this.colour = colour;
+
+    this.blobDiv = document.createElement('div');
+    this.blobDiv.classList.add('blob');
+    document.getElementById('game-display').appendChild(this.blobDiv);
   }
 
   getAbsVel() {
     return Math.sqrt(Math.pow(this.velocity[0], 2) + Math.pow(this.velocity[1], 2));
+  }
+
+  updateDiv() {
+    this.blobDiv.style.left = this.position[1] + 'px';
+    this.blobDiv.style.bottom = this.position[0] + 'px';
+    this.blobDiv.style.height = this.radius*2 + 'px';
+    this.blobDiv.style.width = this.radius*2 + 'px';
   }
 
   // This function updates the blobs position from it's current speed and position 
@@ -31,18 +41,45 @@ class Blob {
     this.velocity[1] += force[1];
   }
 
+  update() {
+    this.move();
+    this.viscosity();
+    this.updateDiv();
+  }
+
   // This function checks if two blobs are contacting each other
   static checkContact (a,b) {
     return a.radius + b.radius <= Math.sqrt(Math.pow(a.position[0] - b.position[0], 2) + Math.pow(a.position[1] - b.position[1], 2)); 
   }
 }
 
-var blobs = [];
 
-document.addEventListener("DOMContentLoaded", function() {
+
+
+
+
+
+var blobs = [];
+var t;
+var fps = 50;
+var initialSize = 50,
+    initialPos = [50,50],
+    initialSpeed = [10,10];
+
+window.onload = function() {
   var playerBlob = new Blob(
-    10,
-    [50, 50],
-    [3,3],
+    initialSize,
+    initialPos,
+    initialSpeed
   );
-});
+  blobs.push(playerBlob);
+  iteration();
+};
+
+function iteration() {
+  for (var i = 0; i < blobs.length; i++) {
+    blobs[i].update();
+  }
+
+  t=setTimeout("iteration()",1000/fps);
+}
