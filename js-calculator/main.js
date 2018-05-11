0cb1721132db4951bc90864a9d6b2ed9e2271443
Stanley Roache <stan.roache@gmail.com>
2018-05-12 08:26:46 +1200

    refactored bracket loop

--- js-calculator/main.js
+++ js-calculator/main.js
@@ -1,4 +1,4 @@
-var stack = [],
+var entries = [],
     currentTotal = 0,
     temp = '',
     operators = {
@@ -24,51 +24,60 @@ $('document').ready(function() {
       temp += val;
     // the button pressed is not part of a number
     } else {
-      // add whatever is in temp to stack
-      if (temp) stack.push(temp);
+      // add whatever is in temp to entries
+      if (temp) entries.push(temp);
       // AC button - clear all calculator contents
       if (val === 'AC') {
-        stack = [];
+        entries = [];
         temp = '';
         currentTotal = 0;
       // CE button, clear last entry
       } else if (val == 'CE') {
-        stack.pop();
+        entries.pop();
       // Any operator button
       } else if (operators.hasOwnProperty(val)) {
-        if (stack.length == 0) stack.push(currentTotal);
-        stack.push(val);
+        if (entries.length == 0) entries.push(currentTotal);
+        entries.push(val);
       // = button - evaluate calculation
       } else if (val === '=') {
-        currentTotal = evaluate(stack) || currentTotal;
-        stack = [];
+        currentTotal = evaluate(entries) || currentTotal;
+        entries = [];
       // '(' or ')'
       } else if (val == ')' || val == '(') {
-        stack.push(val);
+        entries.push(val);
       }
       temp = '';
     }
     // display calculation string so far
-    $("#display").html((stack.join('') + temp).slice(-20) || currentTotal);
+    $("#display").html((entries.join('') + temp).slice(-20) || currentTotal);
   });
 });
 
 // takes array of entries and evaluates
-function evaluate(stack) {
+function evaluate(entries) {
+  entries = dealWithBracketPair(entries);
+  // perform operations by order of precedence
+  entries = performOperationByType(entries, ['^']);
+  entries = performOperationByType(entries, ['x', '/']);
+  entries = performOperationByType(entries, ['+','-']);
+  return (entries.length == 1) ? '' + entries.pop() : "something's wrong";
+}
+
+function dealWithBracketPair(entries) {
   let outerExpression = [],
       innerExpression = [];
   // this while loop checks for brackets and prioritises the contained expression 
   // by passing it to another instance of evaluate
-  while (stack.length > 0) {
-    if (stack[0] == '(') {
+  while (entries.length > 0) {
+    if (entries[0] == '(') {
       // get rid of bracket
-      let current = stack.shift();
+      let current = entries.shift();
       // iniate variable to track nested brackets if any
       let counter = 1;
       // push everything inside bracket pair to inner expression
       while (counter && current) {
         // get next entry
-        current = stack.shift();
+        current = entries.shift();
         // adjust nested bracket counter
         if (current == '(') counter++;
         else if (current == ')') counter--;
@@ -78,28 +87,24 @@ function evaluate(stack) {
       outerExpression.push(evaluate(innerExpression));
     } else {
       // send the element to the outer expression
-      outerExpression.push(stack.shift()); 
+      outerExpression.push(entries.shift()); 
     }
   }
-  // perform operations by order of precedence
-  stack = performOperationByType(outerExpression, ['^']);
-  stack = performOperationByType(stack, ['x', '/']);
-  stack = performOperationByType(stack, ['+','-']);
-  return (stack.length == 1) ? '' + stack.pop() : "something's wrong";
+  return outerExpression;
 }
 
-// this loops over the stack and performs the specified operations
-function performOperationByType(stack, types) {
-  let newStack = [];
-  for (let i = 0; i < stack.length; i++) {
+// this loops over the entries and performs the specified operations
+function performOperationByType(entries, types) {
+  let newentries = [];
+  for (let i = 0; i < entries.length; i++) {
     // check if the next entry matches up with one of the specified operators
-    if (types.indexOf(stack[i]) != -1) {
-      // pop the last operand off the new stack and do the operation by looking it up in the operators object
-      newStack.push(operators[stack[i]](newStack.pop(),stack[i+1]));
+    if (types.indexOf(entries[i]) != -1) {
+      // pop the last operand off the new entries and do the operation by looking it up in the operators object
+      newentries.push(operators[entries[i]](newentries.pop(),entries[i+1]));
       // skip forward once to account for the second operand
       i++;
-    // otherwise push the next operand onto the new stack
-    } else newStack.push(stack[i]);
+    // otherwise push the next operand onto the new entries
+    } else newentries.push(entries[i]);
   }
-  return newStack;
+  return newentries;
 }
